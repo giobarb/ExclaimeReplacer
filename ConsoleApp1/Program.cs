95986783f440ad06ed74ba60de18689c5e5d0317
giobarb <giobarbak@gmail.com>
2021-09-27 22:21:58 +0200

    This is an alpha version.
    First release, this is a working program, still no installer need to work on that, but I at least have a working application.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,17 +15,22 @@ using System.Threading;
 using System.Net;
 using System.Windows.Forms;
 using System.Reflection;
+using Microsoft.Win32;
 
 namespace GraphAPIConsole
 {
+    //JSONConfig class so I can use work with the config easier.
     class JSONConfig
     {
         public string tenantID;
         public string clientID;
         public string clientSecret;
         public string email;
-        public string signatureAddress;
+        public string signatureAddressHTML;
+        public string signatureAddressTXT;
+        public string XMLFilesZip;
         public string encapsulator;
+        public bool firstRun;
 
         public bool LoadConfig(string pathToConfig)
         {
@@ -40,8 +45,11 @@ namespace GraphAPIConsole
                     clientID = JSONConfig.clientID;
                     clientSecret = JSONConfig.clientSecret;
                     email = JSONConfig.email;
-                    signatureAddress = JSONConfig.signatureAddress;
+                    signatureAddressHTML = JSONConfig.signatureAddressHTML;
+                    signatureAddressTXT = JSONConfig.signatureAddressTXT;
+                    XMLFilesZip = JSONConfig.XMLFilesZip;
                     encapsulator = JSONConfig.encapsulator;
+                    firstRun = JSONConfig.firstRun;
                 }
                 //try parsing JSONObject, if it works parse it and return true.
                 catch
@@ -52,21 +60,24 @@ namespace GraphAPIConsole
             }
             return false;
         }
+        public void RewriteFirstRun(string pathToConfig)
+        {
+            this.firstRun = false;
+        }
     }
 
     class Program
     {
-        [STAThread]
         static void Main(string[] args)
         {
             //Getting path to my AppData folder.
-            string pathToAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Test"); 
+            string pathToAppData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Test");
             JSONConfig jSONConfig = new JSONConfig();
             //If a config exists, continue and log it.
             if (jSONConfig.LoadConfig(pathToAppData + "\\config.json"))
             {
                 WriteLog(pathToAppData, "Found config, continuing");
-            } 
+            }
             //If a config doesn't exist, log it and exit, retry on next run.
             else
             {
@@ -106,37 +117,106 @@ namespace GraphAPIConsole
                 System.Environment.Exit(0);
             }
 
-            //try downloading the file, if it fails log and exit.
+            //try downloading the files, if it fails log and exit.
             try
             {
                 using (var client = new WebClient())
                 {
-                    client.DownloadFile(jSONConfig.signatureAddress, pathToAppData + "\\test.html");
+                    client.DownloadFile(jSONConfig.signatureAddressHTML, pathToAppData + "\\test.html");
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
-                WriteLog(pathToAppData, $"couldn't download the file in \"{jSONConfig.signatureAddress}\" check network path and connection");
+                WriteLog(pathToAppData, $"couldn't download the file in \"{jSONConfig.signatureAddressHTML}\" check network path and connection");
                 Environment.Exit(0);
             }
 
-            //downloading the HTML file to use for signatures
+            //downloading the files to use for signatures
             using (var client = new WebClient())
             {
-                client.DownloadFile(jSONConfig.signatureAddress, pathToAppData + "\\signature.html");
+                client.DownloadFile(jSONConfig.signatureAddressHTML, pathToAppData + "\\signature.html");
             }
 
-            string searchWord = null;
-            string rawSignatureString = System.IO.File.ReadAllText(pathToAppData + "\\signature.html");
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(jSONConfig.signatureAddressHTML, pathToAppData + "\\signature.txt");
+            }
 
-            // foreach Property in SigningUser, this is to reaplce %%displayName%% into an actual display name, if the value of it is null log ang
-            // in the html,rtf and txt signatures if this is not gamer I don't know what is
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(jSONConfig.signatureAddressHTML, pathToAppData + "\\XMLZipFiles.zip");
+            }
+
+            string rawSignatureString = System.IO.File.ReadAllText(pathToAppData + "\\signature.html");
+            rawSignatureString = RawReplacer(pathToAppData,rawSignatureString, jSONConfig, signingUser);
+            System.IO.File.WriteAllText(pathToAppData + "\\signature.html", rawSignatureString);
+
+            #region HTML to RTF, doesn't work yet.
+            //Console.WriteLine(signingUser.JobTitle);
+
+            //string convertFromToRtf = System.IO.File.ReadAllText(pathToAppData + "\\signature.html"); // html content
+            //RichTextBox rtbTemp = new RichTextBox();
+            //WebBrowser wb = new WebBrowser();
+            //wb.Navigate("about:blank");
+            //wb.Document.Write(convertFromToRtf);
+
+            //while (wb.ReadyState != WebBrowserReadyState.Complete)
+            //{
+            //    Thread.Sleep(200);
+            //    System.Windows.Forms.Application.DoEvents();
+            //}
+
+            //wb.Document.ExecCommand("SelectAll", false, null);
+            //wb.Document.ExecCommand("Copy", false, null);
+            //rtbTemp.SelectAll();
+            //rtbTemp.Paste();
+            //convertFromToRtf = rtbTemp.ToString();
+            //rtbTemp.SaveFile(pathToAppData + "\\signature.rtf", RichTextBoxStreamType.RichText);
+            //string a = wb.Document.ToString();
+            #endregion
+            //I don't actually need an RTF signature, it works with HTM only, so I only need to create a .txt signature and it should be baller
+            //I do need to create an RTF signature, the .htm file has a load of Microsoft bullshit Imma ignore it, so does the %%signatureName%%_files folder,
+            //it's a bunch of shit I don't know what is, but it works if I copy it, so it should be gucci.
+
+            //Rewrite registryKey only on firstRun of this program, add FirstRun to conf.json
+            string keyName = "SOFTWARE\\Microsoft\\Office\\16.0\\Outlook\\Profiles\\Outlook\\9375CFF0413111d3B88A00104B2A6676";
+            RegistryKey keyForOutlookSignature = Registry.CurrentUser.OpenSubKey(keyName);
+            string[] subkeys = keyForOutlookSignature.GetSubKeyNames();
+
+            //If firstRun is set to true I am rewriting the registry entry for the signatures
+            if (jSONConfig.firstRun)
+            {
+                WriteLog(pathToAppData, "performing firstRun");
+                foreach (string subkeyNames in subkeys)
+                {
+                    WriteLog(pathToAppData, $"Currently comparing {subkeyNames}");
+                    RegistryKey checkingRegistry = Registry.CurrentUser.OpenSubKey(keyName + $"\\{subkeyNames}",true);
+                    if ((string)checkingRegistry.GetValue("Account Name") == jSONConfig.email)
+                    {
+                        WriteLog(pathToAppData,"Found the right address to change keys for.");
+                        checkingRegistry.SetValue("5New Signature", "signature");
+                        checkingRegistry.SetValue("Reply-Forward Signature", "signature");
+                    }
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine(DateTime.Now.ToLongDateString());
+            Console.ReadLine();
+
+        }
+
+        public static string RawReplacer(string pathToAppData, string rawSignatureString, JSONConfig jSONConfig, User signingUser)
+        {
+            string searchWord = null;
+            // foreach Property in SigningUser, this is to replace %%displayName%% into an actual display name, if the value of it is null log and exit
+
             foreach (PropertyInfo propInfo in signingUser.GetType().GetProperties())
             {
                 Console.WriteLine(propInfo.Name + " - " + propInfo.GetValue(signingUser, null));
                 //if the property is included in the signature.html I will execute the code inside
                 searchWord = Encapsulate(propInfo.Name, jSONConfig.encapsulator);
-                if (rawSignatureString.Contains(searchWord)) 
+                if (rawSignatureString.Contains(searchWord))
                 {
                     //checking if the value of the property is null, if it is then log and exit
                     if (propInfo.GetValue(signingUser, null) == null)
@@ -147,29 +227,13 @@ namespace GraphAPIConsole
                     // if it isn't null I will replace and exit
                     else
                     {
-                        WriteLog(pathToAppData,$"{propInfo.Name.ToString()} replacing with {propInfo.GetValue(signingUser, null)}");
+                        WriteLog(pathToAppData, $"{propInfo.Name.ToString()} replacing with {propInfo.GetValue(signingUser, null)}");
                         rawSignatureString = rawSignatureString.Replace(searchWord, propInfo.GetValue(signingUser, null).ToString());
                     }
                 }
             }
 
-            System.IO.File.WriteAllText(pathToAppData + "\\signature.html",rawSignatureString);
-            //Console.WriteLine(signingUser.JobTitle);
-
-            string convertFromToRtf = System.IO.File.ReadAllText(pathToAppData + "\\signature.html"); // html content
-            RichTextBox rtbTemp = new RichTextBox();
-            WebBrowser wb = new WebBrowser();
-            wb.Navigate("about:blank");
-            wb.Document.Write(convertFromToRtf);
-            wb.Document.ExecCommand("SelectAll", false, null);
-            wb.Document.ExecCommand("Copy", false, null);
-            rtbTemp.SelectAll();
-            rtbTemp.Paste();
-            convertFromToRtf = rtbTemp.ToString();
-            
-            Console.WriteLine(DateTime.Now.ToLongDateString());
-            Console.ReadLine();
-            
+            return rawSignatureString;
         }
 
         //if no log exists, create one, if it does append text
@@ -204,7 +268,7 @@ namespace GraphAPIConsole
 
             var users = await graphClient.Users
                         .Request()
-                        .Select("displayName,jobTitle,mobilePhone,mail")
+                        .Select("*")
                         .GetAsync();
             return users.ToList();
         }
